7986a46446b8fe93a2201e1aeb502b7ba784d6de
LokeshMamidipalle <lokeshmamidipalle7044@gmail.com>
2026-02-13 09:11:58 +0530

    resume ai project

---
+++ app.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, UploadFile, File
+import PyPDF2
+
+app = FastAPI()
+
+skills_list = [
+    "python",
+    "java",
+    "sql",
+    "machine learning",
+    "aws"
+]
+
+@app.post("/upload")
+async def upload_resume(file: UploadFile = File(...)):
+    
+    pdf_reader = PyPDF2.PdfReader(file.file)
+
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+
+    text = text.lower()
+
+    found_skills = []
+
+    for skill in skills_list:
+        if skill in text:
+            found_skills.append(skill)
+
+    return {"skills_found": found_skills}
